a9942061b4ef7fa602e40d98428d7d6a923a73b1
TurtlePower <agnkvdm@gmail.com>
2023-04-02 15:01:21 +0300

    add color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+console.dir(refs.startBtn);
+
+const CHANGE_COLOR_TIME = 1000;
+let intervalId = null;
+refs.startBtn.disabled = false;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_TIME);
+}
+
+function onStopBtnClick() {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
